3318e83ea9f38f40e76937d53a05928d6aefa46b
Ilan Schnell <ilanschnell@gmail.com>
2019-12-01 12:59:03 -0600

    rename variable

--- 059.py
+++ 059.py
@@ -3,11 +3,11 @@ from string import ascii_lowercase as lowercase
 
 with open('cipher.txt') as fi:
     s = fi.read()
-crypt = [int(x) for x in s.split(',')]
+cipher = [int(x) for x in s.split(',')]
 
 def try_decrypt(password, display=False):
-    key = map(ord, (500 * password)[:len(crypt)])
-    new = [c[0] ^ c[1] for c in zip(crypt, key)]
+    key = map(ord, (500 * password)[:len(cipher)])
+    new = [c[0] ^ c[1] for c in zip(cipher, key)]
     text = ''.join(map(chr, new))
     if display:
         print(password, sum(new))
