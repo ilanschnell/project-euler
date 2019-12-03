9754670f5dc46a236736d7ffad8668a0b7f8a988
Ilan Schnell <ilanschnell@gmail.com>
2019-12-03 15:20:47 -0600

    add 142

---
+++ 142.py
@@ -0,0 +1,38 @@
+from math import sqrt
+from bitarray import bitarray
+
+is_sqr = bitarray(1_000_001)
+is_sqr.setall(False)
+for i in range(1001):
+    is_sqr[i * i] = True
+
+def prop(x, y):
+    return is_sqr[x + y] and is_sqr[x - y]
+
+pa = {}
+
+M = 1000
+for stp in range(2, M, 2):
+    for sxmy in range(1, M):
+        sxpy = sxmy + stp
+        if sxpy < M:
+            xmy = sxmy * sxmy
+            xpy = sxpy * sxpy
+            x = (xmy + xpy) // 2
+            y = x - xmy
+            if x in pa:
+                pa[x].append(y)
+            else:
+                pa[x] = [y]
+
+sk = sorted(pa)
+for x in sk:
+    pa[x] = pa[x][::-1]
+
+for x in sk:
+    ys = pa[x]
+    if len(ys) >= 2:
+        for i, y in enumerate(ys[:-1]):
+            for z in ys[i + 1:]:
+                if prop(x, z) and prop(y, z):
+                    print(x + y + z)
