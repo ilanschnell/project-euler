f1adb24595cfd1a58cf3ab15efdbaed024476712
Ilan Schnell <ilanschnell@gmail.com>
2019-12-03 15:38:17 -0600

    add 102

---
+++ 102.py
@@ -0,0 +1,18 @@
+from math import acos, sqrt, pi
+
+
+def prod2(a, b):
+    return sum(a[i] * b[i] for i in range(2))
+
+def gamma(a, b):
+    return acos(prod2(a, b) / sqrt(prod2(a, a) * prod2(b, b)))
+
+cnt = 0
+for line in open('triangles.txt'):
+    data = [int(i) for i in line.split(',')]
+    a = data[0:2]
+    b = data[2:4]
+    c = data[4:6]
+    if gamma(a, b) + gamma(b, c) + gamma(c, a) > 2 * pi - 1e-10:
+        cnt += 1
+print(cnt)
