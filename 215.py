fa2b3d6919c3f4d8053b0f0bb8c168a0f4610c6f
Ilan Schnell <ilanschnell@gmail.com>
2019-12-04 14:58:47 -0600

    use recursive generate_rows()

--- 215.py
+++ 215.py
@@ -5,20 +5,22 @@ from functools import lru_cache
 WIDTH = 32
 HEIGHT = 10
 
-def generate_rows():
-    result = set()
-    for s in product(range(2), repeat=WIDTH // 2):
-        bricks = [3 if k else 2 for k in s]
-        row = []
-        p = 0
-        for w in bricks:
-            p += w
-            if p > WIDTH:
-                break
-            if p == WIDTH:
-                result.add(frozenset(row))
-            row.append(p)
-    return sorted(result)
+all_rows = []
+
+def generate_rows(row):
+    row = list(row)
+    w = row[-1] if row else 0  # current width
+    if w + 2 == WIDTH or w + 3 == WIDTH:  # just one brick left?
+        all_rows.append(row)
+        return
+    if w + 1 == WIDTH:  # gap too small for a brick?
+        return
+    # add a small brick
+    row.append(w + 2)
+    generate_rows(row)
+    # replace last 2-brick by 3-brick
+    row[-1] += 1
+    generate_rows(row)
 
 def compatible(r1, r2):
     for i in r1:
@@ -26,14 +28,14 @@ def compatible(r1, r2):
             return False
     return True
 
-rows = generate_rows()
-Nrows = len(rows)
+generate_rows([])
+Nrows = len(all_rows)
 print('rows', Nrows)
 
 compatible_rows = defaultdict(set)
 for i in range(Nrows):
     for j in range(i):
-        if compatible(rows[i], rows[j]):
+        if compatible(all_rows[i], all_rows[j]):
             compatible_rows[i].add(j)
             compatible_rows[j].add(i)
 print("compatible_rows done")
