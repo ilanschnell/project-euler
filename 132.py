fd3387b8f7b20d7ac3fa28ae6fb0312ef02d19d9
Ilan Schnell <ilanschnell@gmail.com>
2019-12-05 12:51:11 -0600

    cleanup

--- 132.py
+++ 132.py
@@ -5,11 +5,11 @@ for a prime factor p:
 
 -> R(k) = 0       (mod p)
 -> 10^k - 1 = 0   (mod 9 * p)
--> 10^k = 1       (mod 9 * p)    where k = 1000_000_000
+-> 10^k = 1       (mod 9 * p)    where k = 10^9
 """
 from sympy.ntheory import primerange
 
-k = 1000_000_000
+k = 1_000_000_000
 s = n = 0
 for p in primerange(1, 1000_000):
     if pow(10, k, 9 * p) == 1:
