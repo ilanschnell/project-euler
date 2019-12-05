529b299ea2e207961a9f53c821dee83bd608f889
Ilan Schnell <ilanschnell@gmail.com>
2019-12-04 20:52:34 -0600

    add a few comments

--- 287.py
+++ 287.py
@@ -14,15 +14,20 @@ def is_black(x, y):
 def encode(x0, y0, x1, y1):
     if x0 == x1:
         assert y0 == y1
-        return 2
+        return 2  # color doesn't matter, always 2 bits
 
+
+    # same color on all four corner -> the whole area is covered by the same
+    # color however, this assumption doesn't hold on the first level
     if ((is_black(x0, y0) == is_black(x1, y0) ==
          is_black(x1, y1) == is_black(x0, y1)) and x1 - x0 < size - 1):
         return 2
 
+    # optimisaztion: if a 2x2 area needs to be split, then it always
+    # requires 9 bits (a split bit and 4 color bits)
     if x0 + 1 == x1:
         assert y0 + 1 == y1
-        return 1 + 4 * 2
+        return 9
 
     half = (x1 - x0 + 1) // 2
     return (
