ed6c4b5c3d06fc99867b3064a75f17e62dc06c5a
Ilan Schnell <ilanschnell@gmail.com>
2019-12-05 17:06:49 -0600

    add 14

---
+++ 014.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+
+/* return Collatz chain length for given start value */
+long chain_len(long n)
+{
+    int result = 0;
+
+    while (n != 1) {
+        n = n % 2 ? 3 * n + 1 : n / 2;
+        result++;
+    }
+    return result;
+}
+
+int main(void)
+{
+    long m = 0;                 /* max chain length */
+    long result;
+    long i, k;
+
+    for (i = 1; i < 1000000; i++) {
+        k = chain_len(i);
+        if (k > m) {
+            m = k;
+            result = i;
+        }
+    }
+    printf("%ld\n", result);
+
+    return 0;
+}
