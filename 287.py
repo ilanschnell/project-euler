4f73e3cfb209dd68d1fe9ce71b71d5e33bdf8b59
Ilan Schnell <ilanschnell@gmail.com>
2019-12-04 20:45:52 -0600

    avoid is_first argument

--- 287.py
+++ 287.py
@@ -11,13 +11,13 @@ def is_black(x, y):
     return dx * dx + dy * dy <= threshold
 
 @njit
-def encode(x0, y0, x1, y1, is_first=True):
+def encode(x0, y0, x1, y1):
     if x0 == x1:
         assert y0 == y1
         return 2
 
     if ((is_black(x0, y0) == is_black(x1, y0) ==
-         is_black(x1, y1) == is_black(x0, y1)) and not is_first):
+         is_black(x1, y1) == is_black(x0, y1)) and x1 - x0 < size - 1):
         return 2
 
     if x0 + 1 == x1:
@@ -26,10 +26,10 @@ def encode(x0, y0, x1, y1, is_first=True):
 
     half = (x1 - x0 + 1) // 2
     return (
-        encode(x0,        y0 + half, x1 - half, y1, False) +
-        encode(x0 + half, y0 + half, x1       , y1, False) +
-        encode(x0,        y0       , x1 - half, y1 - half, False) +
-        encode(x0 + half, y0       , x1       , y1 - half, False) +
+        encode(x0,        y0 + half, x1 - half, y1) +
+        encode(x0 + half, y0 + half, x1       , y1) +
+        encode(x0,        y0       , x1 - half, y1 - half) +
+        encode(x0 + half, y0       , x1       , y1 - half) +
         1)
 
 print(encode(0, 0, size - 1, size - 1))
