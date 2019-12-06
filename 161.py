3a73c89aada43571e50c61dd67134499f2140260
Ilan Schnell <ilanschnell@gmail.com>
2019-12-05 22:07:23 -0600

    works: 5.5 sec

--- 161.py
+++ 161.py
@@ -1,79 +1,76 @@
 from functools import lru_cache
+from bitarray import bitarray as ba, frozenbitarray as fba
 
 # grid size
-height = 9
-width = 2
+height = 12
+width = 9
 
 # indicate an empty row
-EmptyRow = 0
+EmptyRow = fba(width * [0])
 
 # set a certain bit to one (= position is not available anymore),
 # return true if bit was zero
 def use(pos, row):
-    mask = 1 << pos
-    res = bool((row & mask) == 0)
-    row |= mask
+    res = not row[pos]
+    row[pos] = 1
     return res
 
-@lru_cache
+@lru_cache(1 << 25)
 def count(rowsLeft, rowA, rowB, rowC):
     if rowsLeft == 0:
         return 1
 
     # filled another row ?
-    fullRow = (1 << width) - 1
-    if rowA == fullRow:
+    if rowA.count() == width:
         return count(rowsLeft - 1, rowB, rowC, EmptyRow)
 
     # find first gap in rowA
-    pos = 0
-    while (rowA & (1 << pos)) != 0:
-        pos += 1
+    pos = rowA.index(0)
 
     result = 0
 
     # shape: ##
     #        #
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 2 and pos < width - 1 and
             use(pos, a) and use(pos + 1, a) and use(pos, b)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     # shape: ##
     #         #
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 2 and pos < width - 1 and
             use(pos, a) and use(pos + 1, a) and use(pos + 1, b)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     # shape: #
     #        ##
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 2 and pos < width - 1 and
             use(pos, a) and use(pos, b) and use(pos + 1, b)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     # shape:  #
     #        ##
     # note: this shape extends one "negative" unit to the left
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 2 and pos > 0 and pos < width and
              use(pos, a) and use(pos - 1, b) and use(pos, b)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     # shape:  #
     #         #
     #         #
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 3 and pos < width and
             use(pos, a) and use(pos, b) and use(pos, c)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     # shape: ###
-    a, b, c = rowA, rowB, rowC
+    a, b, c = ba(rowA), ba(rowB), ba(rowC)
     if (rowsLeft >= 1 and pos < width - 2 and
             use(pos, a) and use(pos + 1, a) and use(pos + 2, a)):
-        result += count(rowsLeft, a, b, c)
+        result += count(rowsLeft, fba(a), fba(b), fba(c))
 
     return result
 
