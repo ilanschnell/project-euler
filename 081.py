e891dc239ce8b5d2b31668ab0336700e89b09485
Ilan Schnell <ilanschnell@gmail.com>
2019-12-16 01:34:10 -0600

    simplify edges

--- 081.py
+++ 081.py
@@ -1,35 +1,35 @@
-import networkx
+from networkx import DiGraph, shortest_path_length
 
 matrix = [[int(s) for s in line.split(',')] for line in open('files/p081_matrix.txt')]
 n = len(matrix)
 
 # Problem 81
 
-G = networkx.DiGraph()
+G = DiGraph()
 for i in range(n):
     for j in range(n):
         w = matrix[i][j]
-        G.add_edge((i, j - 1), (i, j), weight=w)  # right
-        G.add_edge((i - 1, j), (i, j), weight=w)  # down
+        G.add_edge((i, j), (i, j + 1), weight=w)  # right
+        G.add_edge((i, j), (i + 1, j), weight=w)  # down
 
-source = -1, 0
-target = n - 1, n - 1
-print('p81:', networkx.shortest_path_length(G, source, target, weight='weight'))
+source = 0, 0
+target = n, n - 1
+print('p81:', shortest_path_length(G, source, target, weight='weight'))
 
 # Problem 82
 
 for i in range(n):
-    G.add_edge('source', (i, -1), weight=0)
-    G.add_edge((i, n - 1), 'target', weight=0)
+    G.add_edge('source', (i, 0), weight=0)
+    G.add_edge((i, n), 'target', weight=0)
     for j in range(n):
-        G.add_edge((i + 1, j), (i, j), weight=matrix[i][j])  # up
+        G.add_edge((i, j), (i - 1, j), weight=matrix[i][j])  # up
 
-print('p82:', networkx.shortest_path_length(G, 'source', 'target', weight='weight'))
+print('p82:', shortest_path_length(G, 'source', 'target', weight='weight'))
 
 # Problem 83
 
 for i in range(n):
     for j in range(n):
-        G.add_edge((i, j + 1), (i, j), weight=matrix[i][j])  # left
+        G.add_edge((i, j), (i, j - 1), weight=matrix[i][j])  # left
 
-print('p83:', networkx.shortest_path_length(G, source, target, weight='weight'))
+print('p83:', shortest_path_length(G, source, target, weight='weight'))
