d4508d29ccd0171f13bc7101c9cb20f955a20c68
Ilan Schnell <ilanschnell@gmail.com>
2019-12-16 01:21:13 -0600

    remove duplicate code, add comments

--- 081.py
+++ 081.py
@@ -9,8 +9,8 @@ G = networkx.DiGraph()
 for i in range(n):
     for j in range(n):
         w = matrix[i][j]
-        G.add_edge((i, j - 1), (i, j), weight=w)
-        G.add_edge((i - 1, j), (i, j), weight=w)
+        G.add_edge((i, j - 1), (i, j), weight=w)  # right
+        G.add_edge((i - 1, j), (i, j), weight=w)  # down
 
 source = -1, 0
 target = n - 1, n - 1
@@ -18,29 +18,18 @@ print('p81:', networkx.shortest_path_length(G, source, target, weight='weight'))
 
 # Problem 82
 
-G = networkx.DiGraph()
 for i in range(n):
     G.add_edge('source', (i, -1), weight=0)
     G.add_edge((i, n - 1), 'target', weight=0)
     for j in range(n):
-        w = matrix[i][j]
-        G.add_edge((i, j - 1), (i, j), weight=w)
-        G.add_edge((i - 1, j), (i, j), weight=w)
-        G.add_edge((i + 1, j), (i, j), weight=w)
+        G.add_edge((i + 1, j), (i, j), weight=matrix[i][j])  # up
 
-print('p82', networkx.shortest_path_length(G, 'source', 'target', weight='weight'))
+print('p82:', networkx.shortest_path_length(G, 'source', 'target', weight='weight'))
 
 # Problem 83
 
-G = networkx.DiGraph()
 for i in range(n):
     for j in range(n):
-        w = matrix[i][j]
-        G.add_edge((i, j - 1), (i, j), weight=w)
-        G.add_edge((i, j + 1), (i, j), weight=w)
-        G.add_edge((i - 1, j), (i, j), weight=w)
-        G.add_edge((i + 1, j), (i, j), weight=w)
+        G.add_edge((i, j + 1), (i, j), weight=matrix[i][j])  # left
 
-source = -1, 0
-target = n - 1, n - 1
 print('p83:', networkx.shortest_path_length(G, source, target, weight='weight'))
