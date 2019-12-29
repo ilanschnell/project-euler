5b977e339c2af67272841da133e774ae71fd3d50
Ilan Schnell <ilanschnell@gmail.com>
2019-12-28 23:17:38 -0600

    speedup

--- 026.py
+++ 026.py
@@ -1,18 +1,17 @@
-from decimal import getcontext, Decimal
+from itertools import count
 
-precision = 5000
-getcontext().prec = precision
+def recur_length(d):
+    r = 1
+    seen = {}
+    for i in count(0):
+        if r == 0:
+            return 0
+        if r in seen:
+            return i - seen[r]
+        seen[r] = i
+        r = (10 * r) % d
 
-def recur(d):
-    f = str(Decimal(1) / Decimal(d))
-    if len(f) <= precision:
-        return 0
-    for r in range(1, 1000):
-        if f[-2*r:-r] == f[-3*r:-2*r] == f[-4*r:-3*r]:
-            #print(d, r, f[-2*r:-r])
-            return r
+assert recur_length(7) == 6
 
-assert recur(7) == 6
-
-len, d = max((recur(d), d) for d in range(2, 1000))
+length, d = max((recur_length(d), d) for d in range(2, 1000))
 print(d)
