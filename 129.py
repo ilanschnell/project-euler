c98d60569ca3fcd7868b12fdafa9881fd23e2385
Ilan Schnell <ilanschnell@gmail.com>
2020-01-20 10:13:21 -0600

    remove unused code

--- 129.py
+++ 129.py
@@ -3,7 +3,6 @@ from math import gcd
 
 
 def A(n):
-    assert gcd(n, 10) == 1
     for k in count(1):
         if pow(10, k, 9 * n) == 1:
             return k
@@ -14,8 +13,6 @@ for n in count(1000000):
     if gcd(n, 10) != 1:
         continue
     a = A(n)
-    if a is None:
-        continue
     print('A(%s) = %s' % (n, a))
     if a > 1000000:
         break
