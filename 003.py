cf39be46e2b3b85e5f1487f4a8b372d48b3284f5
Ilan Schnell <ilanschnell@gmail.com>
2022-10-01 01:10:16 -0500

    simplify problem 3

--- 003.py
+++ 003.py
@@ -3,20 +3,22 @@ from math import sqrt
 from bitarray import bitarray
 
 
-N = 1000_000
+N = 10_000
 a = bitarray(N + 1)
 a.setall(True)
 a[:2] = False
 for i in range(2, int(sqrt(N)) + 1):
     if a[i]:  # i is prime, so all multiples are not
         a[i*i::i] = False
-primes = a.search(1)
-
 
 n = 600851475143
 while n > 1:
-    for p in primes:
+    for p in a.itersearch(1):
         if n % p == 0:
             print(p)
             n //= p
             break
+    else:
+        raise ValueError("not enough primes")
+
+assert n == 1
