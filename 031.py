7eb8d35abe2289f1d8854437fa76add61787bee1
Ilan Schnell <ilanschnell@gmail.com>
2025-02-11 00:13:05 -0600

    simplify (requires Python 3.9)

--- 031.py
+++ 031.py
@@ -1,9 +1,9 @@
-from functools import lru_cache
+from functools import cache
 
 coins = [200, 100, 50, 20, 10, 5, 2, 1]
 Ncoins = len(coins)
 
-@lru_cache(1 << 16)
+@cache
 def count(amount, maxcoin=0):
     """
     Given some amount of money, and the index of the largest coin to use,
