eb52f849c095943174a3a963b89d48952f83ad4f
Ilan Schnell <ilanschnell@gmail.com>
2025-02-12 16:23:36 -0600

    remove unused code

--- 031.py
+++ 031.py
@@ -9,8 +9,6 @@ def count(amount, maxcoin=0):
     Given some amount of money, and the index of the largest coin to use,
     return the number of combinations the amount can be made.
     """
-    if maxcoin == Ncoins - 1:
-        return 1
     res = 0
     for i in range(maxcoin, Ncoins):
         amount_left = amount - coins[i]
